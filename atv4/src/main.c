35c8e5cf015507327f6deff48955ea6cede4e252
Flavio-Note <flaben10@gmail.com>
2026-05-27 13:00:38 -0300

    update atv4

--- atv4/src/main.c
+++ atv4/src/main.c
@@ -1,5 +1,5 @@
 #include "MKL25Z4.h"
-
+// vou usata a porta PTB2 ou A2
 void delayMs (int n) {
 	volatile int i;
 	volatile int j;
@@ -10,16 +10,28 @@ void delayMs (int n) {
 
 void main(void)
 {
-    //habilita o clock
-    SIM_SCGC5 |= (1 << 10);
+    //habilita o clock na porta B
+    SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK;
 
-    PORTB_PCR19 &= ~(1 << 10);
-    PORTB_PCR19 &= ~(1 << 9);
-    PORTB_PCR19 |= (1 << 8);
-    //habilita o led vermelho como saida
-    GPIOB_PDDR |= (1 << 19);
+    //liga o mux para gpio da porta 2
+    //PORTB->PCR[2] &= ~PORT_PCR_MUX_MASK;
+    PORTB->PCR[2] |= PORT_PCR_MUX(1);
 
-    GPIOB_TSOR |= (1 << 19);
+    //habilita o clock do adc 
+    SIM->SCGC6 |= SIM_SCGC6_ADC0_MASK;
+
+    // tipo de trigger ADTRG
+    ADC0->SC2 |= ADC_SC2_ADTRG_MASK;
+
+    // fonte do clock e resolucao
+    ADC0->CFG1 |= ADC_CFG1_ADICLK(3); //Asynchronous clock (ADACK)
+    ADC0->CFG1 |= ~ADC_CFG1_MODE_MASK; // zera a resolucao -> 8 bit
+
+    //define o canal 7 como de entrada
+    ADC0->SC1[0] &= ~ADC_SC1_ADCH_MASK; //zera a mascara
+    ADC0->SC1[0] |= ADC_SC1_ADCH(7); // canal 7
+
+    
     while (1) {
         //espera 1 segundo
         delayMs(500);
