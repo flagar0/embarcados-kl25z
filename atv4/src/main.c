26151cb1c6fe32a8f8782e077697dd551ef3ee83
flaviomidea <flaben10@gmail.com>
2026-05-28 15:15:27 -0300

    update

--- atv4/src/main.c
+++ atv4/src/main.c
@@ -1,4 +1,7 @@
 #include "MKL25Z4.h"
+#include <zephyr.h>
+#include <sys/printk.h>
+#include <stdio.h>
 // vou usata a porta PTB2 ou A2
 void delayMs (int n) {
 	volatile int i;
@@ -14,7 +17,7 @@ void main(void)
     SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK;
 
     //liga o mux para gpio da porta 2
-    //PORTB->PCR[2] &= ~PORT_PCR_MUX_MASK;
+    PORTB->PCR[2] &= ~PORT_PCR_MUX_MASK; //zera mux
     PORTB->PCR[2] |= PORT_PCR_MUX(1);
 
     //habilita o clock do adc 
@@ -31,16 +34,16 @@ void main(void)
     ADC0->SC1[0] &= ~ADC_SC1_ADCH_MASK; //zera a mascara
     ADC0->SC1[0] |= ADC_SC1_ADCH(7); // canal 7
 
-    
+    uint16_t saida;
     while (1) {
-        //espera 1 segundo
-        delayMs(500);
-        GPIOB_TSOR |= (1 << 19);
-        delayMs(500);
-        //desliga led vermelho
-        GPIOB_TSOR |= (1 << 19);
-        //liga led verde
-        //GPIOB_PDOR |= (1 << 19);
+        // espera a flag coco
+        if (NXP_FLD2VAL(ADC_SC1_COCO, ADC0->SC1[0]) == 1) {
+            //salva o resultado
+            saida = ADC0->R[0];
+            printk("SAIDA: %d\n", saida);
+        }
+
+
 
     }
 }
